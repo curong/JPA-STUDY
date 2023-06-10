5a95a18e2e0744f7e008180f7e445af6058b36c8
curong <curong@outlook.kr>
2023-06-11 00:57:11 +0900

    QueryDSL Study Start

---
+++ src/main/java/com/example/querydsl/web/FoodRController.java
@@ -0,0 +1,11 @@
+package com.example.querydsl.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/food")
+@RequiredArgsConstructor
+public class FoodRController {
+}
